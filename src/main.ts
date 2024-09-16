b30fb2d6654b0655bae76855152e25781a2e3926
Hưng Nguyễn Sỹ <hungnguyensy2003@gmail.com>
2024-09-16 15:06:30 +0700

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 declare const module: any;
-
+import * as express from 'express'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.json())
+  app.use(express.urlencoded({extended: true}));
 
   await app.listen(3000, () => {
     console.log(`Server is running on port: 3000`);
